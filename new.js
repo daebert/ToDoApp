3cbfe69416a9c94fc953eb31001037d2cd980cc1
daniel.ebert <daniel.ebert@web.de>
2021-04-28 16:39:23 +0200

    taskList

--- new.js
+++ new.js
@@ -23,11 +23,48 @@
 // //   alert(taskNameInput.value);
 // // };
 
+// const formElement = document.querySelector(".form");
+
+// formElement.onsubmit = function (event) {
+//   event.preventDefault();
+//   const taskDateInput = document.querySelector(".radio-group__input:checked");
+//   const taskNameInput = document.querySelector(".textentry");
+
+//   console.log(taskNameInput.value, taskDateInput.value);
+// };
+
 const formElement = document.querySelector(".form");
 
 formElement.onsubmit = function (event) {
   event.preventDefault();
   const taskDateInput = document.querySelector(".radio-group__input:checked");
   const taskNameInput = document.querySelector(".textentry");
-  console.log(taskNameInput.value, taskDateInput.value);
+
+  if (!taskNameInput.value) {
+    alert("Please enter task's name!");
+    return;
+  }
+  if (!taskDateInput) {
+    alert("Please enter task's date!");
+    return;
+  }
+
+  const task = {
+    name: taskNameInput.value,
+    date: taskDateInput.value,
+  };
+
+  // 1. const taskList = ???; localStorage.getItem, parse?
+
+  const getTaskList = localStorage.getItem("taskList");
+  const taskListParse = JSON.parse(getTaskList);
+
+  // 2) taskListParse.?? (task): wie erweitere ich das array? Append item
+
+  taskListParse.push(task);
+
+  // 3) localStorage.setItem(???); setItem+stringify
+
+  const taskListJSON = JSON.stringify(taskListParse);
+  localStorage.setItem("taskList", taskListJSON);
 };
